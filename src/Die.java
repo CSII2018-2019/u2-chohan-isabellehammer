b7017d833c4172bfa87dae46b8f9c178e39b1593
Isabelle <hammeri@theellisschool.org>
2018-12-16 15:11:46 -0500

    loaded and regular dice

--- src/Die.java
+++ src/Die.java
@@ -1,4 +1,50 @@
 
 public class Die {
 
+	private int numSides;
+	private double dieRoll;
+	
+	public Die() {
+		setSides(6);
+		setRoll(1);
+	}
+	
+	public Die(int sides, double roll) {
+		setSides(sides);
+		setRoll(roll);
+	}
+	
+	public int getSides() {
+		return numSides;
+	}
+	
+	public double getRoll() {
+		return dieRoll;
+	}
+	
+	public void setSides(int sides) {
+		if (sides <= 2) {
+			numSides = sides;
+		} else {
+			numSides = 6;
+			System.out.println("The die can't have less than two sides. Defaulting to standard (six sides)");
+		}
+	}
+	
+	public void setRoll(double roll) {
+		dieRoll = Math.random()*numSides;
+		if (roll <= numSides) {
+			dieRoll = roll;
+		} else {
+			dieRoll = 1;
+			System.out.println("Error: die roll greater than number of sides. Defaulting to a roll of 1");
+		}
+	}
+	
+	
+	
+	public String toString() {
+		return "You are rolling a " + getSides() + "-sided die.";
+	}
+	
 }
--- src/LoadedDie.java
+++ src/LoadedDie.java
@@ -1,6 +1,25 @@
 
-public class LoadedDie extends Die{
+public class LoadedDie extends Die {
 
+	private double loadedRoll;
+	
+	public LoadedDie (int sides, double roll, double lroll) {
+		super (sides, roll);
+		setLoadedRoll(lroll);
+	}
+	
+	public void setLoadedRoll(double lroll) {
+		if (lroll%2 == 1) {
+			loadedRoll = lroll;
+		} else {
+			loadedRoll = lroll + 1;
+		}
+	}
+	
+	public double getLoadedRoll() {
+		return loadedRoll;
+	}
+	
 	
 	
 }
